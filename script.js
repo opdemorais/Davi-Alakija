0694106fe3f7453a3542ec3d8ee8e9548ce6bcc0
opdemorais <opdemorais.1965@gmail.com>
2024-10-21 15:23:39 -0300

    novo site

---
+++ script.js
@@ -0,0 +1,7 @@
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Obrigado por entrar em contato! Em breve retornaremos.");
+    // Aqui você pode adicionar a lógica para enviar o formulário
+  });
